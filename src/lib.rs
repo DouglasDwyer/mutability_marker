67f1d9ba3ef05532acfb669c8faf2992e1479804
Douglas Dwyer <35274124+DouglasDwyer@users.noreply.github.com>
2024-02-10 12:52:10 -0500

    Add associated type for getting references from Mutability

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,8 @@
 //! This crate provides two common marker types, `Const` and `Mut`, for use in distinguishing
 //! between mutable and immutable program state.
 
+use core::ops::*;
+
 /// Marks an immutable type.
 #[derive(Copy, Clone, Debug, Default, PartialEq, Eq, PartialOrd, Ord, Hash)]
 pub struct Const;
@@ -12,10 +14,18 @@ pub struct Const;
 pub struct Mut;
 
 /// Marks whether a type is mutable or immutable.
-pub trait Mutability: private::MutabilityInner {}
+pub trait Mutability: private::MutabilityInner {
+    /// The reference type associated with this mutability.
+    type Ref<'a, T: 'a + ?Sized>: Deref<Target = T>;
+}
 
-impl Mutability for Const {}
-impl Mutability for Mut {}
+impl Mutability for Const {
+    type Ref<'a, T: 'a + ?Sized> = &'a T;
+}
+
+impl Mutability for Mut {
+    type Ref<'a, T: 'a + ?Sized> = &'a mut T;
+}
 
 /// Hides implementation details.
 mod private {
